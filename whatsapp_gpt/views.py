7a7ce8362418f1f683e0699a8ccad7f11dc30ff4
sharanji <sharanjipoun@gmail.com>
2023-09-16 16:11:01 +0530

    speech to text on progress

--- whatsapp_gpt/views.py
+++ whatsapp_gpt/views.py
@@ -5,6 +5,7 @@ from rest_framework.views import APIView
 from heyoo import WhatsApp
 import requests
 from .api_key import *
+from .voices import ProcessAudio
 
 
 class OpenAIGPTView(APIView):
@@ -14,7 +15,7 @@ class OpenAIGPTView(APIView):
         self.fb_version = "v15.0"
         self.phone_id = "918610711834"
         self.sender_id = "116480298220877"
-        self.access_token = "EAAMAp6fxdaEBOZBcVNIJyZBcIArWceedWtontTyMOS1e4tIDTA5knC9ghvEkxKF3jsFmKtASFCtZAcKmCRW4KaP082Y9oU03pTpnrIZCirJqKiJ23krHlTBoFJCtJLFwUUQhE5YkhtKdbiUDyt15ZAZCfzo6ODPZChX8RAZCNKHcJG6bCr53CtVn7XI9sN2UmZA7RrJB9GA8ZCmPwDhi7Ue1QZD"
+        self.access_token = "EAAMAp6fxdaEBO5l3oDBe9c8mdZBavxRDvlmWh2L2mAz2ZBiZAs0KEflDwlO8pb5MyZBA821fTZBbqXqVIs1B0HY7COj15FZBAkEZAkC8WxswWzJ5XNNdeEIwaMc3M01CanZCoAKmpxBalnwNNQtK1U8IADPOvV8VkwOWZBFud2KpQmSzjO2xZAkdlhLXQxNerD4l43KjFM8nYvSq9dc0L289gZDD"
 
     def get(self, request):
         answer = "hello boss"
@@ -31,20 +32,28 @@ class OpenAIGPTView(APIView):
             messenger = WhatsApp(self.access_token, phone_number_id=self.sender_id)
             changed_field = messenger.changed_field(request.data)
             if changed_field == "messages":
-                getUserinput = messenger.get_message(request.data)
+                message_type = messenger.get_message_type(request.data)
+                if message_type == "audio":
+                    getUserinput = ProcessAudio().getTextfromAudio(
+                        messenger, jsonData=request.data, access_token=self.access_token
+                    )
+
+                elif message_type == "text":
+                    getUserinput = messenger.get_message(request.data)
+
                 message_id = messenger.get_message_id(request.data)
                 user_number = messenger.get_mobile(request.data)
 
-                input = getUserinput
-                openai.api_key = self.OPENAI_API_KEY
-                completion = openai.ChatCompletion.create(
-                    model="gpt-3.5-turbo", messages=[{"role": "user", "content": input}]
-                )
-                answer = completion["choices"][0]["message"]["content"]
+                # input = getUserinput
+                # openai.api_key = self.OPENAI_API_KEY
+                # completion = openai.ChatCompletion.create(
+                #     model="gpt-3.5-turbo", messages=[{"role": "user", "content": input}]
+                # )
+                # answer = completion["choices"][0]["message"]["content"]
 
-                r = messenger.reply_to_message(
-                    message_id=message_id, message=answer, recipient_id=user_number
-                )
+                # r = messenger.reply_to_message(
+                #     message_id=message_id, message=answer, recipient_id=user_number
+                # )
 
-                return Response(r)
+                return Response(getUserinput)
         return Response("Bad Request", status=200)
--- whatsapp_gpt/voices.py
+++ whatsapp_gpt/voices.py
@@ -1,15 +1,56 @@
 import speech_recognition as sr
 import pyttsx3
+from pydub import AudioSegment
 
-# Initialize the recognizer
-r = sr.Recognizer()
+# audio2 = sr.AudioFile("D:\django\whatsapp_gpt\whatsapp_gpt\sample.wav")
 
-audio2 = sr.AudioFile("D:\django\whatsapp_gpt\whatsapp_gpt\sample.wav")
+# with audio2 as source:
+#     audio = r.record(source)
 
-with audio2 as source:
-    audio = r.record(source)
+# MyText = r.recognize_google(audio)
+# MyText = MyText.lower()
 
-MyText = r.recognize_google(audio)
-MyText = MyText.lower()
+# print("Did you say ", MyText)
+from rest_framework.response import Response
+import os
+from django.conf import settings
+from django.http import HttpResponse, Http404
+import requests
+import subprocess
 
-print("Did you say ", MyText)
+
+class ProcessAudio:
+    def getTextfromAudio(self, messenger, jsonData, access_token):
+        audio = messenger.get_audio(jsonData)
+        audio_id, mime_type = audio["id"], audio["mime_type"]
+        audio_url = messenger.query_media_url(audio_id)
+        audio_filename = messenger.download_media(
+            audio_url, mime_type, file_path="audios"
+        )
+        # response = requests.request(
+        #     "GET",
+        #     audio_url,
+        #     headers={"Authorization": f"Bearer {access_token}"},
+        #     data={},
+        # )
+        # media_file = open(f"audios/{audio_id}.wav", "wb")
+        # media_file.write(response.content)
+        # media_file.close()
+
+        # Initialize the recognizer
+        r = sr.Recognizer()
+
+        subprocess.run(
+            f"ffmpeg -i audios/865375815144174.mp3 audios/wav/865375815144174.wav",
+            shell=True,
+        )
+        # sound = AudioSegment.from_mp3("audios/865375815144174.mp3")
+        # sound.export("audios/wav/865375815144174.wav", format="wav")
+
+        audio_file = sr.AudioFile(f"audios/865375815144174.mp3")
+        # audio_file = sr.AudioFile(f"audios/{audio_id}.wav")
+        with audio_file as source:
+            recorded_audio = r.record(source)
+        extracted_text = r.recognize_google(recorded_audio)
+
+        return extracted_text
